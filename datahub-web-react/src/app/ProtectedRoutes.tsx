03b0bfcb917e88df441875a00d5ade563a077663
Imindzzz <xd199153@gmail.com>
2024-08-30 03:38:27 +0000

    feat: 修改数据 分栏

--- datahub-web-react/src/app/ProtectedRoutes.tsx
+++ datahub-web-react/src/app/ProtectedRoutes.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Layout } from 'antd';
-import { HomePage } from './home/HomePage';
+// import { HomePage } from './home/HomePage';
 import { SearchRoutes } from './SearchRoutes';
 import EmbedRoutes from './EmbedRoutes';
 import { PageRoutes } from '../conf/Global';
+import Landing from './landing';
 
 /**
  * Container for all views behind an authentication wall.
@@ -13,7 +14,8 @@ export const ProtectedRoutes = (): JSX.Element => {
     return (
         <Layout>
             <Switch>
-                <Route exact path="/" render={() => <HomePage />} />
+                {/* <Route exact path="/" render={() => <HomePage />} /> */}
+                <Route exact path="/" render={() => <Landing />} />
                 <Route path={PageRoutes.EMBED} render={() => <EmbedRoutes />} />
                 <Route path="/*" render={() => <SearchRoutes />} />
             </Switch>
--- datahub-web-react/src/app/landing/index.tsx
+++ datahub-web-react/src/app/landing/index.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import { PageRoutes } from '../../conf/Global';
 import imageBg from './images/bg.png';
 import imageLogo from './images/logo.png';
 import imageTop1 from './images/top1.png';
@@ -5,8 +7,12 @@ import imageTop2 from './images/top2.png';
 import imageTop3 from './images/top3.png';
 import imageTop4 from './images/top4.png';
 import { Banner, Bottom, Main, Title, Top } from './styles';
+import * as QueryString from 'query-string';
+import { useHistory } from 'react-router';
 
 export default function () {
+  const history = useHistory();
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <Main>
       <Banner
@@ -20,8 +26,25 @@ export default function () {
           <div className="gap"></div>
           <div className="subTitle">Wilmar Data Catalog</div>
           <div className="search">
-            <input className="input" placeholder="请输入表/字段/业务对象…" />
-            <div className="button">搜索</div>
+            <input ref={inputRef} className="input" placeholder="请输入表/字段/业务对象…" />
+            <div className="button" onClick={() => {
+              const query = inputRef?.current?.value || '';
+              // /search?page=1&query=%25E4%25BD%25A0%25E5%25A5%25BD&unionType=0
+              const search = QueryString.stringify(
+                {
+                  // ...filtersToQueryStringParams(constructedFilters),
+                  query: encodeURIComponent(query),
+                  page: 1,
+                  unionType: 0,
+                  // sortOption: selectedSortOption,
+                },
+                // { arrayFormat: 'comma' },
+              );
+              history.push({
+                pathname: `${PageRoutes.SEARCH}`,
+                search,
+              });
+            }}>搜索</div>
           </div>
         </div>
       </Banner>
@@ -30,11 +53,11 @@ export default function () {
           {
             id: '0',
             img: imageTop1,
-            name: '数据源',
-            num: 1,
+            name: '平台',
+            num: 8,
             subItems: [
-              { id: '00', name: '数据平台', disabled: false },
-              { id: '01', name: '未来：BW', disabled: true },
+              { id: '00', name: '涉及营销、运营、供应链、生产、物流、财务、主数据、数据共8个平台', disabled: false },
+              { id: '01', name: '未来：供应链金融、采购、质量、人事、公共', disabled: true },
             ],
           },
           {
@@ -43,8 +66,8 @@ export default function () {
             name: '来源系统',
             num: 18,
             subItems: [
-              { id: '10', name: 'EPMDMSSAPCCP……', disabled: false },
-              { id: '11', name: '未来：LOG 3.0', disabled: true },
+              { id: '10', name: 'SAP 、DMS 、EPM 、CCP……', disabled: false },
+              { id: '11', name: '未来：WMS 、LIMS…', disabled: true },
             ],
           },
           {
@@ -52,7 +75,9 @@ export default function () {
             img: imageTop3,
             name: '表',
             num: 1704,
-            subItems: [{ id: '20', name: 'SAP:256个', disabled: false }],
+            subItems: [
+              { id: '20', name: 'SAP:256个', disabled: false },
+              { id: '21', name: '…', disabled: false }],
           },
           {
             id: '3',
@@ -60,7 +85,7 @@ export default function () {
             name: '字段',
             num: 54029,
             subItems: [
-              { id: '30', name: '已补充系统描述：？个', disabled: false },
+              { id: '30', name: '已补充系统描述：xx个(来源系统梳理中）', disabled: false },
               { id: '31', name: '...', disabled: true },
             ],
           },
@@ -92,7 +117,7 @@ export default function () {
                           className={subItem.disabled ? 'item disabled' : 'item'}
                         >
                           <span className="dot"></span>
-                          <span>{subItem.name}</span>
+                          <span className="text">{subItem.name}</span>
                         </div>
                       );
                     })}
@@ -116,11 +141,15 @@ export default function () {
             name: '营销域',
             num: 11,
             subItems: [
-              { id: '00', name: 'Sell Out/Sell in、经销商库存', disabled: false },
-              { id: '01', name: '终端客户、标准客户', disabled: false },
-              { id: '02', name: 'POS销量/库存', disabled: false },
-              { id: '03', name: '固定任务', disabled: false },
-              { id: '04', name: '滚动计划、销售预测', disabled: true },
+              { id: '00', name: '终端客户、标准客户', disabled: false },
+              { id: '01', name: '经销商客销售、库存', disabled: false },
+              { id: '02', name: '订单、提单、发票', disabled: false },
+              { id: '03', name: 'POS销量、库存', disabled: false },
+              { id: '04', name: '固定任务', disabled: false },
+
+              { id: '05', name: '滚动计划', disabled: true },
+              { id: '05', name: '销售预测', disabled: true },
+              { id: '06', name: '…', disabled: true },
             ],
           },
           {
@@ -128,10 +157,16 @@ export default function () {
             name: '供应链域',
             num: 6,
             subItems: [
-              { id: '10', name: '要货计划', disabled: false },
-              { id: '11', name: '采购订单', disabled: false },
-              { id: '12', name: '出入库明细', disabled: false },
-              { id: '13', name: '寻源、采购绩效', disabled: true },
+              { id: '04', name: '要货计划', disabled: false },
+              { id: '04', name: '采购订单', disabled: false },
+              { id: '04', name: '出入库明细', disabled: false },
+              { id: '04', name: '账面库存（SAP）', disabled: false },
+
+              { id: '05', name: '意向', disabled: true },
+              { id: '05', name: '寻源', disabled: true },
+              { id: '05', name: '绩效', disabled: true },
+              { id: '05', name: '生产排产', disabled: true },
+              { id: '05', name: '…', disabled: true },
             ],
           },
           {
@@ -140,10 +175,13 @@ export default function () {
             num: 2,
 
             subItems: [
-              { id: '20', name: '一卡通车辆进出场、异常日志', disabled: false },
-              { id: '21', name: '运单', disabled: true },
-              { id: '22', name: '仓储', disabled: true },
-              { id: '23', name: '送货地址', disabled: true },
+              { id: '04', name: '实物库存（ WMS ）', disabled: false },
+              { id: '04', name: '现场管理（一卡通）', disabled: false },
+
+              { id: '05', name: '运单', disabled: true },
+              { id: '05', name: '仓储', disabled: true },
+              { id: '05', name: 'POI', disabled: true },
+              { id: '05', name: '…', disabled: true },
             ],
           },
           {
@@ -151,11 +189,15 @@ export default function () {
             name: '生产域',
             num: 3,
             subItems: [
-              { id: '30', name: '生产订单', disabled: false },
-              { id: '31', name: '生产产出', disabled: false },
-              { id: '32', name: '工艺路线', disabled: true },
-              { id: '33', name: '质检', disabled: true },
-              { id: '34', name: '人员排班', disabled: true },
+              { id: '04', name: '生产订单', disabled: false },
+              { id: '04', name: '生产产出（MES)', disabled: false },
+              { id: '04', name: '工厂点位指标（PIMS)', disabled: false },
+
+              { id: '05', name: 'BOM', disabled: true },
+              { id: '05', name: '工艺路线', disabled: true },
+              { id: '05', name: '质检', disabled: true },
+              { id: '05', name: '人员排班', disabled: true },
+              { id: '05', name: '…', disabled: true },
             ],
           },
           {
@@ -164,10 +206,13 @@ export default function () {
             num: 4,
 
             subItems: [
-              { id: '40', name: '报销单抬头、明细、流程审批、凭证信息', disabled: false },
-              { id: '41', name: '总账', disabled: true },
-              { id: '42', name: '往来明细', disabled: true },
-              { id: '43', name: '产品成本分析', disabled: true },
+              { id: '04', name: '财务共享：报销单抬头、明细、流程审批、凭证信息', disabled: false },
+
+              { id: '05', name: '总账', disabled: true },
+              { id: '05', name: '往来明细', disabled: true },
+              { id: '05', name: '产品成本分析', disabled: true },
+              { id: '05', name: '资金', disabled: true },
+              { id: '05', name: '…', disabled: true },
             ],
           },
           {
@@ -175,37 +220,58 @@ export default function () {
             name: '数据域',
             num: 10,
             subItems: [
-              {
-                id: '50',
-                name: '主数据信息（物料、客户、供应商、公司组织架构、通用基础项）',
-                disabled: false,
-              },
-              { id: '51', name: '工厂人员信息', disabled: false },
-              { id: '52', name: '规则资产', disabled: true },
+              { id: '04', name: '物料基础&管理信息', disabled: false },
+              { id: '04', name: '客户基础信息、销售范围&合作范围信息', disabled: false },
+              { id: '04', name: '供应商基础信息', disabled: false },
+              { id: '04', name: '公司工厂组织架构信息', disabled: false },
+              { id: '04', name: '工厂人员信息', disabled: false },
+
+              { id: '05', name: '总账科目', disabled: true },
+              { id: '05', name: '成本中心', disabled: true },
+              { id: '05', name: '规则资产', disabled: true },
+              { id: '05', name: '…', disabled: true },
             ],
           },
         ].map((item) => {
+
+          const itemA = item.subItems.filter(x => x.disabled);
+          const itemB = item.subItems.filter(x => !x.disabled);
           return (
             <div key={item.id} className="item">
               <div className="wrapper">
                 <div className="right">
                   <div className="title">
-                    <span>{item.name}：</span>
-                    <span className="number">{item.num}</span>
-                    <span>个</span>
+                    <span>{item.name}</span>
+                    {/* <span className="number">：{item.num}</span>
+                    <span>个</span> */}
                   </div>
                   <div className="subItems">
-                    {item.subItems.map((subItem) => {
-                      return (
-                        <div
-                          key={subItem.id}
-                          className={subItem.disabled ? 'item disabled' : 'item'}
-                        >
-                          <span className="dot"></span>
-                          <span>{subItem.name}</span>
-                        </div>
-                      );
-                    })}
+                    <div className="col">
+                      {itemB.map((subItem) => {
+                        return (
+                          <div
+                            key={subItem.id}
+                            className={subItem.disabled ? 'item disabled' : 'item'}
+                          >
+                            <span className="dot"></span>
+                            <span className="text">{subItem.name}</span>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className="col">
+                      {itemA.map((subItem) => {
+                        return (
+                          <div
+                            key={subItem.id}
+                            className={subItem.disabled ? 'item disabled' : 'item'}
+                          >
+                            <span className="dot"></span>
+                            <span>{subItem.name}</span>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
                 </div>
               </div>
